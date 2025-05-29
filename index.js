b6685749b5a48dba465711bfb4b0c832d969a109
Ayush Srivastava <ayushsrivastava1103@gmail.com>
2025-05-29 17:21:11 +0530

    added router for click counts and analytics

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./connect.js");
+const URL = require("./models/url.js");
 
 const app = express();
 const port = 8001;
@@ -12,5 +13,23 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => {
 app.use(express.json());
 
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(port, () => console.log(`Server started at Port : ${port}`));
