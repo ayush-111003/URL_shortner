631e89299cc1bf69eae6c1743ce9dd1464aa338d
Ayush Srivastava <ayushsrivastava1103@gmail.com>
2025-05-29 17:27:27 +0530

    final commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => {
   console.log("MongoDB connected");
 });
 
+//middleware
 app.use(express.json());
 
 app.use("/url", urlRoute);
