2e06e9b94ecd64164dc825718c0906a9efc00cc6
Ayush Srivastava <ayushsrivastava1103@gmail.com>
2025-06-04 18:29:54 +0530

    integrated jwt authentication

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,22 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Piyush$123@$";
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  // console.log("User found:", user);
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) {
+    return null;
+  }
+  return jwt.verify(token, secret);
 }
 
 module.exports = {
